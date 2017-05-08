5a2adc4b9b0869a7d0ddf1c0808e14efdcc38a5e
Strus Sviatoslav <svyatoslav@email.ua>
2017-05-08 20:29:04 +0300

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Strus Sviatoslav on 08.05.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ParserYC/ParserCY.java
@@ -0,0 +1,8 @@
+package ParserYC;
+
+/**
+ * Created by Strus Sviatoslav on 08.05.2017.
+ */
+public class ParserCY {
+
+}
